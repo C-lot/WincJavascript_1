2d291170056e7620d40c82ebac74efffd62f86ce
C-lot <73837008+C-lot@users.noreply.github.com>
2021-06-25 12:20:07 +0200

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,19 @@
+console.log("Hello World");
+let = "Charlotte";
+console.log(let);
+
+const som1 = 4 + 4;
+console.log(som1);
+const som2 = "4" + "4";
+console.log(som2);
+//Want dit is een string
+
+console.log(8 - 2);
+console.log(som1 / 2);
+console.log(33 * 8);
+console.log(90 % 80);
+
+const leeftijd = 34;
+const leeftijdString = "34";
+console.log(typeof leeftijd);
+console.log(typeof leeftijdString);
